80e0672550135b8be747b064de1499f27596e9cc
Morgan Kesler <keslerm@dasbiersec.com>
2013-06-17 18:41:26 -0400

    Moving Schedule service back to service package and increasing run delays to 15 minute

---
+++ src/main/java/com/dasbiersec/sit/spring/service/ScraperScheduleService.java
@@ -0,0 +1,54 @@
+package com.dasbiersec.sit.spring.service;
+
+import com.dasbiersec.sit.spring.alerts.AlertItemQueue;
+import com.dasbiersec.sit.spring.dto.AlertMessageDTO;
+import com.dasbiersec.sit.spring.model.Alert;
+import com.dasbiersec.sit.spring.model.Scraper;
+import com.dasbiersec.sit.spring.service.AlertServices;
+import com.dasbiersec.sit.spring.service.ScraperService;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ScraperScheduleService
+{
+    @Autowired
+    private AlertServices alertServices;
+
+    @Autowired
+    private ScraperService scraperService;
+
+	private Logger log = Logger.getLogger(getClass());
+
+    @Scheduled(fixedRate = 900000)
+	private void process()
+	{
+		List<Scraper> scrapers = scraperService.getAllScrapers();
+
+		log.info("Got " + scrapers.size() + " scrapers");
+
+		for (Scraper scraper : scrapers)
+		{
+			if (scraper.getEnabled() == true)
+			{
+				log.info("Running scraper - " + scraper.getId());
+				scraperService.runScraper(scraper);
+
+			}
+		}
+
+		log.info("Items pending in alert: " + AlertItemQueue.size());
+
+		List<Alert> alerts = alertServices.getAlerts();
+		log.debug("Got " + alerts.size() + " alerts");
+
+		List<AlertMessageDTO> messages = alertServices.processPendingAlerts(alerts);
+		log.info("Got " + messages.size() + " messages");
+
+		alertServices.sendMessages(messages);
+	}
+}
